a76027f552fa8be86bf0f197274cc9a867233477
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-09-06 18:06:01 +0200

    Add project files.

---
+++ Sulmar.AspNetCore.Routing.RouteConstraints/ConstraintsExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Sulmar.AspNetCore.Routing.RouteConstraints
+{
+    public static class ConstraintsExtensions
+    {
+        public static IServiceCollection AddPeselConstraint(this IServiceCollection services)
+        {
+            return services.Configure<RouteOptions>(options => options.ConstraintMap.Add("pesel", typeof(PeselValidatorRouteConstraint)));
+        }
+
+        public static IServiceCollection AddRegonConstraint(this IServiceCollection services)
+        {
+            return services.Configure<RouteOptions>(options => options.ConstraintMap.Add("regon", typeof(RegonValidatorRouteConstraint)));
+        }
+
+        public static IServiceCollection AddNipConstraint(this IServiceCollection services)
+        {
+            return services.Configure<RouteOptions>(options => options.ConstraintMap.Add("nip", typeof(NipValidatorRouteConstraint)));
+        }
+
+        public static IServiceCollection AddPolishConstraints(this IServiceCollection services)
+        {
+            return services
+                .AddPeselConstraint()
+                .AddRegonConstraint()
+                .AddNipConstraint();
+        }
+    }
+
+    
+}
---
+++ Sulmar.AspNetCore.Routing.RouteConstraints/RouteConstraints.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using Validators.Abstractions;
+using Validators.Polish;
+
+namespace Sulmar.AspNetCore.Routing.RouteConstraints
+{
+
+    public class PeselValidatorRouteConstraint : RouteConstraints, IRouteConstraint
+    {
+        protected PeselValidatorRouteConstraint(string key = "pesel") : base(new PeselValidator(), key)
+        {
+        }
+    }
+
+    public class RegonValidatorRouteConstraint : RouteConstraints, IRouteConstraint
+    {
+        protected RegonValidatorRouteConstraint(string key = "regon") : base(new RegonValidator(), key)
+        {
+        }
+    }
+
+    public class NipValidatorRouteConstraint : RouteConstraints, IRouteConstraint
+    {
+        protected NipValidatorRouteConstraint(string key = "nip") : base(new NipValidator(), key)
+        {
+        }
+    }
+
+
+    public abstract class RouteConstraints : IRouteConstraint
+    {
+        private readonly IValidator validator;
+        private readonly string pattern;
+
+        protected RouteConstraints(IValidator validator, string pattern)
+        {
+            this.validator = validator;
+            this.pattern = pattern;
+        }
+
+        public bool Match(HttpContext httpContext, IRouter route, string routeKey, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            if (values.TryGetValue(pattern, out object valueObject))
+            {
+                string value = valueObject.ToString();
+
+                try
+                {
+                    return validator.IsValid(value);
+                }
+                catch (FormatException)
+                {
+                    return false;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    
+}
